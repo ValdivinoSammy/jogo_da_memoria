08ed7732cf2077646ff174d249f06e0022eb7aec
Sammy <valdivinosammyvp@gmail.com>
2026-08-30 20:30:30 -0300

    mudando coisa pra caralho

--- scripts/interface.js
+++ scripts/interface.js
@@ -1,65 +1,114 @@
-let front = document.querySelectorAll(".front");
-let piece = document.querySelectorAll(".card");
+
+
 let winScream = document.getElementById("winScream");
+let start = document.getElementById("start");
+let container = document.getElementById("container");
 
-function again(){
-// colocando as imagens no "verso" da carta
-front.forEach(element => {
-    element.innerHTML = `<img alt="frente da carta" class="imageCards" ${imagens[embaralhado[i]]} ></img>`;
-    i++;
-});
+function again() {
+    let front = document.querySelectorAll(".front");
+    let piece = document.querySelectorAll(".card");
+    let qtdc = whatIsGrid();
+    // colocando as imagens no "verso" da carta
+    front.forEach(element => {
+        element.innerHTML = `<img alt="frente da carta" class="frontCard" ${qtdc[embaralhado[i]]} ></img>`;
+        i++;
+    });
 
-// Adicionando o evento de click nos cards
-piece.forEach(element => {
-    element.addEventListener("click", flip);
-});
+    // Adicionando o evento de click nos cards
+    piece.forEach(element => {
+        element.addEventListener("click", flip);
+    });
 };
-again();
 
 // flipar a carta
 
 function flip(event) {
-    if(!lockedMode){
-    let valor = event.currentTarget.dataset.marcador;
-    if (valor === position[0]) { return }
-    else {
-        position.push(valor);
-        event.currentTarget.style.animation = "flip 600ms forwards";
-        cardsFliped++;
+    if (!lockedMode) {
+        let valor = event.currentTarget.dataset.marcador;
+        if (valor === position[0]) { return }
+        else {
+            position.push(valor);
+            event.currentTarget.style.animation = "flip 600ms forwards";
+            cardsFliped++;
 
 
-        for (let i in embaralhado) {
-            valor === i ? flipeds.push(embaralhado[i]) : null;
+            for (let i in embaralhado) {
+                valor === i ? flipeds.push(embaralhado[i]) : null;
+            }
+            if (cardsFliped === 2) {
+                cardsFliped = 0;
+                moves++;
+                comparar(position[0], position[1]);
+            } else { return };
         }
-        if (cardsFliped === 2) {
-            cardsFliped = 0;
-            moves++;
-            comparar(position[0], position[1]);
-        } else { return };
-    }
     }
 };
 
 // tela de vitoria 
 
 function win() {
-    if(!localStorage.getItem("record") || localStorage.getItem("record") > moves ){
-    localStorage.setItem("record", moves);};
-    let record = localStorage.getItem("record");
+    let record;
+    if(tamanhoDaGrid === 20){let record = records.recordGrid20;}else{record = records.recordGrid30};
+
+    if (!localStorage.getItem(record) || localStorage.getItem(record) > moves) {
+        localStorage.setItem(record, moves);
+    };
+    valorRecord = localStorage.getItem(record);
     setTimeout(() => {
         winScream.classList.add("winDiv");
-        winScream.innerHTML = `<h1>Parabéns, você venceu o Jogo.</h1><p>Recorde de movimentos: ${record}</p><p>Movimentos realizados: ${moves} </p><div id="reset" onclick="restart()">Restart</div>`;
+        winScream.innerHTML = `<h1>Parabéns, você venceu o Jogo.</h1><p>Recorde de movimentos: ${valorRecord}</p><p>Movimentos realizados: ${moves} </p><div id="reset" onclick="restart()">Restart</div><div id="reset" onclick="inicio()">Tela inicial</div>`;
     }, 600);
 };
 
-function winRemove(){
+function winRemove() {
     winScream.classList.remove("winDiv");
     winScream.innerHTML = '';
 };
 
-// desviando as cartas para recomeçar
-function flipDown(){
+// desvirando as cartas para recomeçar
+function flipDown() {
+    let piece = document.querySelectorAll(".card");
     piece.forEach(element => {
-    element.style.animation = "flipDown 600ms forwards";
-})
+        element.style.animation = "flipDown 600ms forwards";
+    })
 };
+
+function starRemoveOurAdd() {
+    if(removeOurAdd){
+    start.id = "";
+    start.innerHTML = "";
+    } else{
+    container.innerHTML = "";
+    start.id = "start";
+    start.innerHTML = `<h1>ESCOLHA O TAMANHO DO SEU TABULEIRO</h1><button onclick="grid20()">20 Cartas</button><button onclick="grid30()">30 Cartas</button>`;
+}
+}
+
+// adicionando as cartas no HTML
+
+function colocando() {
+
+    for (let k = 0; k < tamanhoDaGrid; k++){
+    container.innerHTML += `<div class="card" data-marcador="${k}"><div class="verse"><img alt="verso da carta" class="imageCards" src="./imagens/verso.png"></div><div class="front"></div></div>`;
+}
+}
+
+// formato do tabuleiro na tela
+
+function newGrid(){
+    let piece = document.querySelectorAll(".card");
+    if(tamanhoDaGrid === 30){
+        container.classList.add("gridTemplate30cards");
+        piece.forEach(elemeto=>{
+         elemeto.classList.add("cardMenor");
+        })
+
+    }else{
+        container.classList.add("gridTemplate20cards");
+        piece.forEach(elemeto=>{
+         elemeto.classList.add("cardMaior");
+        })
+
+    }
+}
+
--- scripts/jogo.js
+++ scripts/jogo.js
@@ -1,37 +1,62 @@
 let i = 0;
 let cardsFliped = 0;
+let moves = 0;
+let tamanhoDaGrid;
 let imageN;
 let embaralhado = [];
 let flipeds = [];
 let position = [];
 let lockedMode = false;
-let moves = 0;
-
+let removeOurAdd = true;
 
 let imagens = {
-    "0": 'src="./imagens/html.png"',
-    "1": 'src="./imagens/bootstrap.png"',
-    "2": 'src="./imagens/css.png"',
-    "3": 'src="./imagens/electron.png"',
-    "4": 'src="./imagens/firebase.png"',
-    "5": 'src="./imagens/javascript.png"',
-    "6": 'src="./imagens/jquery.png"',
-    "7": 'src="./imagens/mongo.png"',
-    "8": 'src="./imagens/node.png"',
-    "9": 'src="./imagens/react.png"',
+    cards20 :{
+    "0": 'src="./imagens/Hunt.webp"',
+    "1": 'src="./imagens/Nihility.webp"',
+    "2": 'src="./imagens/Harmony.webp"',
+    "3": 'src="./imagens/Remembrance.webp"',
+    "4": 'src="./imagens/Preservation.webp"',
+    "5": 'src="./imagens/Erudition.webp"',
+    "6": 'src="./imagens/Destruction.webp"',
+    "7": 'src="./imagens/Elation.webp"',
+    "8": 'src="./imagens/Abundance.webp"',
+    "9": 'src="./imagens/Voracity.webp"',},
+    cards30 :{
+    "0": 'src="./imagens/Hunt.webp"',
+    "1": 'src="./imagens/Nihility.webp"',
+    "2": 'src="./imagens/Harmony.webp"',
+    "3": 'src="./imagens/Remembrance.webp"',
+    "4": 'src="./imagens/Preservation.webp"',
+    "5": 'src="./imagens/Erudition.webp"',
+    "6": 'src="./imagens/Destruction.webp"',
+    "7": 'src="./imagens/Elation.webp"',
+    "8": 'src="./imagens/Abundance.webp"',
+    "9": 'src="./imagens/Wind.webp"',
+    "10": 'src="./imagens/Fire.webp"',
+    "11": 'src="./imagens/Lightning.webp"',
+    "12": 'src="./imagens/Ice.webp"',
+    "13": 'src="./imagens/Imaginary.webp"',
+    "14": 'src="./imagens/Quantum.webp"',}
 };
 
+let records = { 
+    recordGrid20:"record",
+    recordGrid30:"recordOutro"};
+
+function whatIsGrid(){
+   return tamanhoDaGrid === 20 ? imagens.cards20 : imagens.cards30;
+}
 
 // Embaralhando as cartas
 function reembaralhar(){
-for (let j = 0; j < 20; j++){
+for (let j = 0; j < tamanhoDaGrid; j++){
     embaralhar();
 };};
 
-reembaralhar();
+
 
 function embaralhar() {
-    imageN = Math.floor(Math.random() * 10);
+    imageN = Math.floor((Math.random()) * (tamanhoDaGrid / 2));
     rep = embaralhado.filter((valor) => valor === imageN);
     if (rep.length == 2) {
         embaralhar();
@@ -41,11 +66,11 @@ function embaralhar() {
 }
 
 
-
 // Comparar cartas viradas
 
 function comparar(pos1, pos2) {
     lockedMode = true;
+    let piece = document.querySelectorAll(".card");
     if (flipeds[0] === flipeds[1] && flipeds[0] !== "" && flipeds[1]!== "" ) {
         embaralhado[pos1] = "";
         embaralhado[pos2] = "";
@@ -88,7 +113,37 @@ function restart(){
     flipDown();
     reembaralhar();
     again();
-
 };
 
+// voltar ao inicio
+function inicio(){
+    moves = 0;
+    i = 0;
+    embaralhado.length = 0;
+    removeOurAdd = false;
+    winRemove();
+    starRemoveOurAdd()
+}
 
+// escolheu o bord
+function grid20(){
+    tamanhoDaGrid = 20;
+    removeOurAdd = true;
+    
+    colocando();
+    newGrid();
+    reembaralhar();
+    again();
+    starRemoveOurAdd()
+}
+
+function grid30(){
+    tamanhoDaGrid = 30;
+    removeOurAdd = true;
+    
+    colocando();
+    newGrid();
+    reembaralhar();
+    again();
+    starRemoveOurAdd()
+}
